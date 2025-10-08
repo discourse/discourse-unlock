873fab7b0238f67b31d8961c5d549a95c0c8ff0d
Jarek Radosz <jarek@cvx.dev>
2025-10-08 19:32:34 +0200

    DEV: Update linting

--- assets/javascripts/discourse/initializers/extend-for-unlock.js
+++ assets/javascripts/discourse/initializers/extend-for-unlock.js
@@ -1,4 +1,4 @@
-import { getOwner } from "@ember/application";
+import { getOwner } from "@ember/owner";
 import { next } from "@ember/runloop";
 import { ajax } from "discourse/lib/ajax";
 import loadScript from "discourse/lib/load-script";
